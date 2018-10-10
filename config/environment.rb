ea3a1ee381bb10b8f585c2fb4bb9eb77ab0e178d
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-10-10 14:30:50 +0100

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,12 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
+#configure :development do
+#  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'lab', pool: 2, username: 'dev', password: 'dev'}#
+#end
+
 require './app'
